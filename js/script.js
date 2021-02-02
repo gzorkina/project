2471c7ca21112311958ee6557b19b994987a1ae7
Galina <example@gmail.com>
2021-02-02 19:53:20 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,41 @@
+"use strict";
+
+/*const obj = {
+  name: "Bob",
+  age: 25,
+  isMarried: false,
+};*/
+//console.log(obj.name, 'age', obj.age);
+
+// let arr = ['test', 'variant', 3, {}, []];
+//console.log(arr['4'], arr[0]);
+
+//const result = confirm("Are you here?");
+//console.log(result);
+
+// const answer = +prompt("Вам есть 18?", "");
+// console.log(answer + 5);
+
+// const answers = [];
+// answers[0] = prompt("Как ваше имя?", "");
+// answers[1] = prompt("Как ваша фамилия?", "");
+// answers[2] = prompt("Сколько вам лет?", "");
+// document.write(answers);
+// console.log(typeof(answers));
+
+// const category = "toys";
+// console.log(`https://othersite.com/${category}/5`);
+
+// const user = "Galina";
+// console.log(`Привет, ${user}`);
+
+// let incr = 10,
+//     decr = 10;
+// incr++;
+// decr--;
+// console.log(++incr);
+// console.log(--decr);
+
+// console.log(5%2);
+
+// console.log(2*4 == 8);
