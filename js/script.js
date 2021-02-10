1e1af4ab27a8450f95440eb92f3d830110ade105
Galina <example@gmail.com>
2021-02-10 22:52:04 +0300

    all changes

--- js/script.js
+++ js/script.js
@@ -40,3 +40,164 @@
 
 // console.log(2*4 == 8);
 
+// const num = 50;
+
+// if (num < 49) {
+//   console.log("error");
+// } if (num > 100) {
+//   console.log("error");
+// } else {
+//   console.log("ok");
+// }
+
+// (num === 50) ? console.log("ok") : console.log("error");
+
+// const num = 50;
+
+// switch (num) {
+//     case 49:
+//       console.log('меньше');
+//       break;
+//     case 100:
+//       console.log('больше');
+//       break;
+//     case 51:
+//       console.log('верно');
+//     break;
+//     default:
+//       console.log('ничего не подходит');
+// }
+
+// let num = 50;
+
+// while (num < 55) {
+//   console.log(num);
+//   num++;
+// }
+
+// do {
+//   console.log(num);
+//   num++;
+// } while (num < 58);
+
+// for (let i = 0; i < 8; i++) {
+//     if (i == 6) {
+//       break;
+//       continue;
+//     }
+//   console.log(num);
+//   num++;
+// }
+
+// let i = 0;
+
+// for (i = 0; i < 3; i++) { // используем существующую переменную
+//   console.log(i); // 0, 1, 2
+// }
+
+// console.log(i);
+
+// let i = 0;
+// while (i++ < 5) console.log( i );
+
+// let num;
+
+// do {
+//   num = prompt("Введите число, большее 100?", 0);
+// } while (num <= 100 && num);
+
+// let num = 20;
+
+// function showFirstMessage(text) {
+//   console.log(text);
+//   num = 10;
+// }
+
+// showFirstMessage("Hello World!");
+
+// console.log(num);
+
+// function calc(a, b) {
+//   return(a + b);
+// }
+
+// console.log(calc(4, 7));
+
+// const logger = function() {
+//   return "hello";
+// };
+
+// alert(logger());
+
+// let addNumber = +prompt("Введите число", "0");
+
+// if (addNumber > 0) {
+//   alert(1);
+// } else if (addNumber < 0) {
+//   alert(-1);
+// } else {
+//   alert(0);
+// }
+
+// let login = prompt("Rnj ds?", "");
+
+// let message = (login == 'Сотрудник') ? 'Привет' : 
+// (login == 'Директор') ? 'Здравствуйте' :
+// (login == '') ? 'Нет логина' : '';
+
+// alert(message );
+
+// let whosThere = prompt("Кто там?", "");
+
+// if (whosThere == "Админ") {
+//   let passName = prompt("Введите пароль", "");
+//   if (passName == "Я главный") {
+//     alert("Здравствуйте!");
+//   } else if (passName == null) {
+//     alert("Отмена");
+//   } else {
+//     alert("Неверный пароль");
+//   }
+// } else if (whosThere == null) {
+//   alert("Отмена");
+// } else {
+//   alert("Я вас не знаю");
+// }
+
+// let sum = 0;
+
+// while (true) {
+//   let value = +prompt("Введите число", "");
+//   if (!value) break;  
+//   sum += value;
+// }
+// alert(sum);
+
+// for (let i = 0; i < 10; i++) {
+//   if (i % 2 == 0) continue;
+//     console.log(i); 
+// }
+
+// let i = 0;
+// while (++i < 5) {console.log( i );}
+
+// let i = 0;
+// while (i++ < 5) {console.log( i );}
+
+// for (let i = 0; i < 5; i++) {console.log( i );}
+// for (let i = 0; i < 5; ++i) {console.log( i );}
+
+// let num;
+// do {
+//   num = prompt("ввести число, большее 100", "");
+// } while (num < 100 && num !== null);
+
+// let n = 10;
+
+// nextPrime: for (let i = 2; i <= n; i++) {
+//   for (let j = 2; j < i; j++) {
+//     if (i % j == 0) continue nextPrime;
+//   }
+//   console.log( i );
+// }
+
--- js/script_project.js
+++ js/script_project.js
@@ -7,15 +7,39 @@ const personalMovieDB = {
   movies: {},
   actors: {},
   genres: [],
-  privat: false
+  private: false
 };
 
-const a = prompt("Один из последних просмотренных фильмов?", ""),
-      b = prompt("На сколько оцените его?", ""),
-      c = prompt("Один из последних просмотренных фильмов?", ""),
-      d = prompt("На сколько оцените его?", "");
+if (personalMovieDB.count < 10) {
+  alert("Просмотрено довольно мало фильмов");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+  alert("Вы классический зритель");
+} else if (personalMovieDB.count >= 30) {
+  alert("Вы киноман");
+} else {
+  alert("Произошла ошибка");
+}
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+let a,
+    b;
+for (let i = 0; i < 2; i++) {
+  do {
+      a = prompt("Один из последних просмотренных фильмов?", "");
+      b = prompt("На сколько оцените его?", "");
+  } while (a == "" || a === null || a.length > 50 || b == "" || b === null || b.length > 50);
+  personalMovieDB.movies[a] = b;
+  
+}
+
+for (let i = 0; i < 2; i++) {
+    const a = prompt("Один из последних просмотренных фильмов?", ""),
+          b = prompt("На сколько оцените его?", "");
+
+    if (a != "" && b != "" && a != null && b != null && a.length < 50 && b.length < 50) {
+        personalMovieDB.movies[a] = b;
+    } else {
+        i--;
+    }
+}
 
 console.log(personalMovieDB);
